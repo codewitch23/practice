27d1babc6065e9a8a2bc4fa3a35348bb32ed5035
codewitch23 <codewitch23@hotmail.com>
2020-06-17 13:40:02 +0430

    auth is create

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,11 @@ use Illuminate\Support\Facades\Route;
  */
 
 
+
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+    Route::get('/',function (){
+        return view('welcome');
+    });
+
+
